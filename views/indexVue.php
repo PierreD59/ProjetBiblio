2bf6d7ee43ec7fd3785e79b0c47377cf167c2766
Pierre Dumont <Pierre>
2019-02-12 19:54:11 +0100

    add front home page

--- views/indexVue.php
+++ views/indexVue.php
@@ -1,11 +1,76 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <title></title>
-  </head>
-  <body>
-    <p>C'est la vue de l'index</p>
-    <?php echo $my_client->getProducts()[0]->getName(); ?>
-  </body>
-</html>
+<div class="page">
+  <?php include 'template/header.php'; ?>
+  <main class="site-content">   
+    <div class="m-0 p-0 row project">
+      
+      <div class="lasted-book">
+        <h1 class="news">Les nouveautés</h1>
+        <div class="list-book row m-0 p-0">
+
+          <div class="m-3 col-sm-5 col-md-6 col-lg-3">
+            <a class="link card" href="#"><img src="../assets/img/livre.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div></a>
+          </div>
+
+          <div class="m-3 col-sm-5 col-md-6 col-lg-3">
+            <a class="link card" href="#"><img src="../assets/img/livre.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div></a>
+          </div>
+
+          <div class="m-3 col-sm-5 col-md-6 col-lg-3">
+            <a class="link card" href="#"><img src="../assets/img/livre.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div></a>
+          </div>
+
+        </div>
+        <a href="#" class="btn button">Voir plus</a>
+      </div>
+
+      <div class="actuality">
+        <h1 class="news">Actualité</h1>
+        <div class="list-actuality row m-0 p-0">
+
+          <div class="col-sm-5 col-md-6 col-lg-3 p-0 mx-3 card">
+            <img class="card-img-top" src="../assets/img/paradis.jpg" alt="Card image cap">
+            <div class="card-body">
+              <h5 class="card-title">Article 1</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#" class="btn btn-primary">En savoir plus...</a>
+            </div>
+          </div>
+
+          <div class="col-sm-5 col-md-6 col-lg-3 p-0 mx-3 card">
+            <img class="card-img-top" src="../assets/img/ecriture.jpg" alt="Card image cap">
+            <div class="card-body">
+              <h5 class="card-title">Article 2</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#" class="btn btn-primary">En savoir plus...</a>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    
+    </div>
+  </main>
+
+  <?php include 'template/footer.php'; ?>
+</div>
+
+        <!-- <a class="m-5 block" href="./index.php?page=mangas">
+          <p class="text-block">Mangas</p>
+        </a>
+    
+        <a class="m-5 block" href="./index.php?page=comics">
+          <p class="text-block">Comics</p>
+        </a>
+    
+        <a class="m-5 block" href="./index.php?page=romans">
+          <p class="text-block">Romans</p>
+        </a> -->
